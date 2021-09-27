833ec8aba580e060ce24f46d85f699e3b776dbd4
Joëlle <72984240+Joelle-Everaert@users.noreply.github.com>
2021-09-27 10:05:38 +0200

    update to deployed 2.0

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import Home from './components/Page/Home'
 import NavBar from './components/foundation/Layout'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 
 function App() {
   return (
     <>
-      <Router>
+     
       <NavBar />
       <div className="container">
-        <Switch>
-          <Route path='/' exact component={Home} />
-        </Switch>
+      <Home />
       </div>
-      </Router>
+
     </>
   )
 }
